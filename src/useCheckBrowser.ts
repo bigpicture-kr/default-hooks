34ba223d85601b66714a76f512f925894880cac7
김민수 <57122180+minsoo-web@users.noreply.github.com>
2022-12-15 16:24:27 +0900

    feat: Add useCheckBrowser browser list (#7)
    * feat: update useCheckBrowser return type
    
    * feat: add useCheckBrowser browser list

--- src/useCheckBrowser.ts
+++ src/useCheckBrowser.ts
@@ -1,12 +1,23 @@
 import { useEffect, useState } from "react";
 
-type Browser = "edge" | "firefox" | "opera" | "safari" | "chrome" | "other";
+type Browser =
+  | "edge"
+  | "firefox"
+  | "opera"
+  | "safari"
+  | "chrome"
+  | "whale"
+  | "brave"
+  | "other"
+  | "ie";
 
 const useCheckBrowser = () => {
   const [browser, setBrowser] = useState<Browser>("other");
 
   useEffect(() => {
-    const userAgent = navigator.userAgent;
+    const userAgent = navigator.userAgent.toLowerCase();
+    const braveNavigator = navigator.brave;
+
     if (userAgent.match(/edg/i)) {
       setBrowser("edge");
     } else if (userAgent.match(/firefox|fxios/i)) {
@@ -17,6 +28,12 @@ const useCheckBrowser = () => {
       setBrowser("chrome");
     } else if (userAgent.match(/safari/i)) {
       setBrowser("safari");
+    } else if (userAgent.match(/whale/i)) {
+      setBrowser("whale");
+    } else if (braveNavigator !== undefined) {
+      setBrowser("brave");
+    } else if (userAgent.search("trident") != -1 || userAgent.match(/msie/i)) {
+      setBrowser("ie");
     } else {
       setBrowser("other");
     }
