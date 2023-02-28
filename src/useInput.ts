d91dbc7347b146dc10ac90a3ccfbe6e209d8c17e
정원 <jwha94@gmail.com>
2023-02-28 10:32:52 +0900

    fix: add useCallback to onChange from useInput

--- src/useInput.ts
+++ src/useInput.ts
@@ -1,4 +1,5 @@
-import { ChangeEvent, Dispatch, SetStateAction, useState } from "react";
+import type { ChangeEvent, Dispatch, SetStateAction } from "react";
+import { useCallback, useState } from "react";
 
 const useInput = (
   initialValue: string,
@@ -25,40 +26,43 @@ const useInput = (
   const [value, setValue] = useState<string>(initialValue);
   const [error, setError] = useState<string>("");
 
-  const onChange = (
-    event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>
-  ) => {
-    const {
-      currentTarget: { value: _value }
-    } = event;
+  const onChange = useCallback(
+    (
+      event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>
+    ) => {
+      const {
+        currentTarget: { value: _value }
+      } = event;
 
-    // 에러였던 요소들 다시 타이핑 시작하면 에러 해제
-    if (error !== "") setError("");
-    if (validators) {
-      let _errorText = "";
+      // 에러였던 요소들 다시 타이핑 시작하면 에러 해제
+      if (error !== "") setError("");
+      if (validators) {
+        let _errorText = "";
 
-      for (let index = 0; index < validators.length; index += 1) {
-        const validator = validators[index];
+        for (let index = 0; index < validators.length; index += 1) {
+          const validator = validators[index];
 
-        if (!validator.validator(_value)) {
-          _errorText = validator.errorText;
-          /*
+          if (!validator.validator(_value)) {
+            _errorText = validator.errorText;
+            /*
            hard: true / error: true -> setValue 안 해준다.
            나머지는 걍 해준다.
           */
-          if (!validator.option?.hard && _errorText !== "") setValue(_value);
-          break;
-        }
+            if (!validator.option?.hard && _errorText !== "") setValue(_value);
+            break;
+          }
 
+          setValue(_value);
+        }
+        if (_errorText !== "") {
+          setError(_errorText);
+        }
+      } else {
         setValue(_value);
       }
-      if (_errorText !== "") {
-        setError(_errorText);
-      }
-    } else {
-      setValue(_value);
-    }
-  };
+    },
+    [error, validators]
+  );
 
   return {
     value: { value, onChange },
